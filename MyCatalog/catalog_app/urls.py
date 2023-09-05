08795aa4061c84eb504cdb7a87ac47322c138601
Sakkrawut Luangvilai <onemorezear@gmail.com>
2023-09-05 11:42:23 +0700

    django Account#

--- MyCatalog/catalog_app/urls.py
+++ MyCatalog/catalog_app/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.homepage, name='homepage'),
     path('product_list', views.product_list, name='product list'),
     path('product_detail', views.product_detail, name='product detail'),
+    path('register_account', views.register_account, name='register_account'),
+    path('transfer', views.transfer, name='transfer'),
 
 ]
